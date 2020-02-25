c8b1ff4718511e821d8cba3c5084366bee934481
Jeremy Kim <thk116@gmail.com>
2020-02-24 17:16:45 -0800

    basic express server working

--- server/app.js
+++ server/app.js
@@ -1,10 +1,32 @@
 // import files and packages up here
+const morgan = require('morgan')
+const express = require('express')
+const data = require('./data.json')
+// body-parser is not necessary if only using GET requests
 
+const PORT = process.env.PORT || 3000;
 
 // create your express server below
-var app;
+const app = express();
 
 // add your routes and middleware below
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+  console.log('hey')
+  res.status(200).send("ok")
+  // res.sendStatus(200)
+});
+
+app.get('/data', (req, res) => {
+  console.log('data')
+  res.json(data);
+});
+
+// app.get('/data', (req, res) => {
+//   console.log('hah!')
+//   res.send()
+// });
 
 // finally export the express application
 module.exports = app;
--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@
 const server = require('./app');
 
 // We're telling the app to listen for network requests on port 3000
+// idiomatic server will separate the listener here from the server in app.js, so the listener is not inside the server
+// everything in the server occurs first and then the listener is added here after
 server.listen(3000, function() {
   console.log('Server is listening on http://localhost:3000');
 });
